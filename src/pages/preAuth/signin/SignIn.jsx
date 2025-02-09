22b9ae8f2d829a21b3d99ba030681b92c37fa0db
Shantanu <badmoreshantanu@gmail.com>
2025-02-09 16:03:43 +0530

    SignIn and SignUp pages

--- src/pages/preAuth/signin/SignIn.jsx
+++ src/pages/preAuth/signin/SignIn.jsx
@@ -1,8 +1,71 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+import { TextField, Button, Container, Typography, Box } from "@mui/material";
+
 function SignIn() {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
+
   return (
-    <div>
-      <h1>SignIn Page</h1>
-    </div>
+    <>
+      <Container maxWidth="sm">
+        <Box sx={{ mt: 5, p: 3, boxShadow: 3, borderRadius: 2 }}>
+          <Typography variant="h4" gutterBottom align="center">
+            Sign In
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              label="Email ID"
+              fullWidth
+              margin="normal"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email address",
+                },
+              })}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+
+            <TextField
+              label="Enter Password"
+              type="password"
+              fullWidth
+              margin="normal"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ mt: 2 }}
+            >
+              Sign In
+            </Button>
+          </form>
+          <DevTool control={control} />
+        </Box>
+      </Container>
+    </>
   );
 }
 
--- src/pages/preAuth/signup/SignUp.jsx
+++ src/pages/preAuth/signup/SignUp.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+import { TextField, Button, Container, Typography, Box } from "@mui/material";
 
 function SignUp() {
-    return (
-        <div>
-            <h1>SignUp Page</h1>
-        </div>
-    )
+    const {
+        register,
+        control,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm();
+    
+      const onSubmit = (data) => {
+        console.log("Form Data:", data);
+        console.log("Register inside onSubmit: ", register);
+      };
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 5, p: 3, boxShadow: 3, borderRadius: 2 }}>
+        <Typography variant="h4" gutterBottom align="center">
+          Sign Up
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            label="First Name"
+            fullWidth
+            margin="normal"
+            {...register("firstName", { required: "First Name is required" })}
+            error={!!errors.firstName}
+            helperText={errors.firstName?.message}
+          />
+          <TextField
+            label="Last Name"
+            fullWidth
+            margin="normal"
+            {...register("lastName", { required: "Last Name is required" })}
+            error={!!errors.lastName}
+            helperText={errors.lastName?.message}
+          />
+          <TextField
+            label="Email ID"
+            fullWidth
+            margin="normal"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "Invalid email address",
+              },
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            label="Mobile No."
+            fullWidth
+            margin="normal"
+            {...register("mobile", {
+              required: "Mobile number is required",
+              pattern: {
+                value: /^[0-9]{10}$/,
+                message: "Invalid mobile number",
+              },
+            })}
+            error={!!errors.mobile}
+            helperText={errors.mobile?.message}
+          />
+          <TextField
+            label="Set Password"
+            type="password"
+            fullWidth
+            margin="normal"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
+          <TextField
+            label="Confirm Password"
+            type="password"
+            fullWidth
+            margin="normal"
+            {...register("confirmPassword", {
+              required: "Confirm Password is required",
+              validate: (value) => value === watch("password") || "Passwords do not match",
+            })}
+            error={!!errors.confirmPassword}
+            helperText={errors.confirmPassword?.message}
+          />
+          <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+            Sign Up
+          </Button>
+        </form>
+        <DevTool control={control} />
+      </Box>
+    </Container>
+  );
 }
 
-export default SignUp
+export default SignUp;
